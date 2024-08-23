471823df933013785f007d40c0a9d2e26065bb4d
Alpha-Ronald <ronnieadewoye@gmail.com>
2024-08-23 14:55:05 +0100

    Firebase option for android

---
+++ lib/firebase/firebase_options.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: "AIzaSyCpbL9l2qDfELlCY_2LMsgUuqKvT1q70mo",
+    appId: "1:897035289106:android:4068419aa5d4b5f5e40ecb",
+    messagingSenderId: "897035289106",
+    projectId: "event-planner-8a49d",
+  );
+}
