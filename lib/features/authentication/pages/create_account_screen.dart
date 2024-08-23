ac1b51e0890a171f543878d1c13363945e0e6c6f
Alpha-Ronald <ronnieadewoye@gmail.com>
2024-08-23 14:57:18 +0100

    Sign_up options screen

--- lib/features/authentication/pages/create_account_screen.dart
+++ lib/features/authentication/pages/create_account_screen.dart
@@ -1,86 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CreateAccountPage extends StatelessWidget {
   const CreateAccountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20.0),
-      width: 300,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            blurRadius: 15.0,
-            offset: Offset(0, 5),
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const Text(
-            'CREATE YOUR ACCOUNT',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          const Text(
-            'This is step 1',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.grey),
-          ),
-          const SizedBox(height: 20.0),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20.w),
+          width: 300.w,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.r),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 15.r,
+                offset: Offset(0, 5),
               ),
-            ),
-            keyboardType: TextInputType.emailAddress,
+            ],
           ),
-          const SizedBox(height: 15.0),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'CREATE YOUR ACCOUNT',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            obscureText: true,
-          ),
-          SizedBox(height: 15.0),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Confirm Password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.0),
+              SizedBox(height: 10.h),
+              const Text(
+                'This is step 1',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey),
               ),
-            ),
-            obscureText: true,
-          ),
-          const SizedBox(height: 20.0),
-          ElevatedButton(
-            onPressed: () {
-            },
-            style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.all(15.0),
-              backgroundColor: Colors.green,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
+              SizedBox(height: 20.h),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
               ),
-            ),
-            child: const Text('Next'),
+              SizedBox(height: 15.h),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.r),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 15.h),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Confirm Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20.h),
+              ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(15.h),
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.r),
+                  ),
+                ),
+                child: const Text('Next'),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
